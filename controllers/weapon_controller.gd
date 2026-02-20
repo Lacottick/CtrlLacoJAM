599f64072c1a07e6ed329cf9e2f5de6892c6d753
MolitorLucas <lucasmolitor10@gmail.com>
2026-02-20 12:05:28 -0300

    feat: Inicio das armas e melhorias visuais

--- controllers/weapon_controller.gd
+++ controllers/weapon_controller.gd
@@ -2,4 +2,4 @@ class_name WeaponController extends Node
 
 func _ready()->void:
 	if get_children().is_empty() or get_children().all(func(c): c is not Weapon):
-		printerr("WEAPON_CONTROLLER PRECISA DE PELO MENOS UM NÓ FILHO DO TIPO WEAPON")
+		push_error("WEAPON_CONTROLLER PRECISA DE PELO MENOS UM NÓ FILHO DO TIPO WEAPON")
